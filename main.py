5ab1fc528f6d5b6efe1793fa1e6dda55172da6c6
AnnaKow123 <kow.anna.poczta@gmail.com>
2022-09-17 18:31:10 +0200

    The basis of the project

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+print('Witaj w grze papier, kamień, nożyce. Wybierz opcję ;)')
+options=['papier', 'kamień', 'nożyce']
+x='tak'
+while x=='tak':
+        
+    player1=input('Gracz 1: ').lower()
+    while player1 not in options:
+        print('Wpisano nieprawidłowe wyrażenie. Wpisz jeszcze raz')
+        player1=input('Gracz 1: ')
+        
+        
+    player2=random.choice(options)
+    print(f'Komputer: {player2}')
+    
+    if player1==player2:
+        print('Remis')
+    else:
+        if player1=='papier':
+            if player2=='kamień':
+                print('Gracz 1 wygrał')
+            else:
+                print('Komputer wygrał')
+        elif player1=='kamień':
+            if player2=='papier':
+                print('Komputer wygrał')
+            else:
+                print('Gracz 1 wygrał')
+        elif player1=='nożyce':
+            if player2=='papier':
+                print('Gracz 1 wygrał')
+            else:
+                print('Komputer wygrał')
+    x=input('Czy kontynuować grę? (Tak/Nie): ').lower()
+
