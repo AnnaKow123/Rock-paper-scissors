00c622b4a91cc4d0f85d826fcf415a0ea6e0aa62
AnnaKow123 <kow.anna.poczta@gmail.com>
2022-09-23 18:35:50 +0200

    Code improvement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing import parent_process
 from PyQt5.QtWidgets import *
 import random
 
@@ -5,60 +6,68 @@ options=['kamień', 'papier', 'nożyce']
 player2=random.choice(options)
 rock, paper, scissors = options 
 
-app=QApplication([])
-window=QWidget()
-window.setWindowTitle('Gra kamień, papier, nożyce')
-message=QLabel('<h3>Witaj w grze papier, kamień, nożyce</h3>', parent=window)
+class WidgetGallery(QDialog):
+    def __init__(self, parent=None):
+        super(WidgetGallery, self).__init__(parent)
 
-button1=QPushButton(rock)
-button2=QPushButton(paper)
-button3=QPushButton(scissors)
+        self.setWindowTitle('Gra kamień, papier, nożyce')
+        message=QLabel('<h3>Witaj w grze papier, kamień, nożyce</h3>', self)
 
-def on_button_rock():
-    alert = QMessageBox()
-    if player2==rock:
-        alert.setText('Komputer wybrał kamień. Remis')
-    else:
-        if player2==paper:
-            alert.setText('Komputer wybrał papier. Komputer wygrał')
-        else:
-            alert.setText('Komputer wybrał nożyce. Wygrałeś!')
-    alert.exec()
+        button1=QPushButton(rock)
+        button2=QPushButton(paper)
+        button3=QPushButton(scissors)
 
-def on_button_paper():
-    alert = QMessageBox()
-    if player2==paper:
-        alert.setText('Komputer wybrał papier. Remis')
-    else:
-        if player2==scissors:
-            alert.setText('Komputer wybrał nożyce. Komputer wygrał')
-        else:
-            alert.setText('Komputer wybrał kamień. Wygrałeś!')
-    alert.exec()
+        def on_button_rock(self):
+            alert = QMessageBox()
+            if player2==rock:
+                alert.setText('Komputer wybrał kamień. Remis')
+            else:
+                if player2==paper:
+                    alert.setText('Komputer wybrał papier. Komputer wygrał')
+                else:
+                    alert.setText('Komputer wybrał nożyce. Wygrałeś!')
+            alert.exec()
 
-def on_button_scissors():
-    alert = QMessageBox()
-    if player2==scissors:
-        alert.setText('Komputer wybrał nożyce. Remis')
-    else:
-        if player2==rock:
-            alert.setText('Komputer wybrał kamień. Komputer wygrał')
-        else:
-            alert.setText('Komputer wybrał papier. Wygrałeś!')
-    alert.exec()
+        def on_button_paper(self):
+            alert = QMessageBox()
+            if player2==paper:
+                alert.setText('Komputer wybrał papier. Remis')
+            else:
+                if player2==scissors:
+                    alert.setText('Komputer wybrał nożyce. Komputer wygrał')
+                else:
+                    alert.setText('Komputer wybrał kamień. Wygrałeś!')
+            alert.exec()
 
-button1.clicked.connect(on_button_rock)
-button2.clicked.connect(on_button_paper)
-button3.clicked.connect(on_button_scissors)
+        def on_button_scissors(self):
+            alert = QMessageBox()
+            if player2==scissors:
+                alert.setText('Komputer wybrał nożyce. Remis')
+            else:
+                if player2==rock:
+                    alert.setText('Komputer wybrał kamień. Komputer wygrał')
+                else:
+                    alert.setText('Komputer wybrał papier. Wygrałeś!')
+            alert.exec()
 
-layout=QVBoxLayout()
-layout.addWidget(message)
-layout.addWidget(button1)
-layout.addWidget(button2)
-layout.addWidget(button3)
-window.setLayout(layout)
+        button1.clicked.connect(on_button_rock)
+        button2.clicked.connect(on_button_paper)
+        button3.clicked.connect(on_button_scissors)
 
-message.show()
-window.show()
-app.exec()
+        layout=QVBoxLayout()
+        layout.addWidget(message)
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+        layout.addWidget(button3)
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    import sys
+    
+    app=QApplication(sys.argv)
+    window=WidgetGallery()
+    #message.show()
+    window.show()
+    app.exec()
 
